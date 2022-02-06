705eead387e8a569a86cfb8ffc2090f5c8e50253
Skyks <61660324+SkyksFloof@users.noreply.github.com>
2022-02-06 00:56:57 +0000

    Word list change
    Changed word list, pretty obvious by the name lol

--- src/main/java/xyz/spicedev/spicecf/listeners/PlayerSwearListener.java
+++ src/main/java/xyz/spicedev/spicecf/listeners/PlayerSwearListener.java
@@ -39,14 +39,14 @@ public class PlayerSwearListener implements Listener {
 
         // All blocked words go here to all you people who want to use this in your own projects
 
-        Collections.addAll(notgudwords, "fuck", "nigga", "shit", "fanny", "penis", "nigger", "ez", "bitch", "5170", "dortware", "dick", "dickhead", "fuk", "cunt", "redesky", "intent.store", "intent store", "rise client", "aether client", "flux client", "moon client", "novoline client", "rose client", "dortware client", "vape client", "vape v4", "vapev4", "vape lite", "bastard", "fuckwit", "arsehole", "asshole", "tit", "twat", "pussy", "cock", "motherfucker", "mother fucker", "wanker");
+        Collections.addAll(notgudwords, "fuck", "nigga", "shit", "fanny", "penis", "nigger", "ez", "bitch", "RealTrippy", "dortware", "dick", "dickhead", "fuk", "cunt", "redesky", "intent.store", "intent store", "rise client", "aether client", "flux client", "moon client", "novoline client", "rose client", "dortware client", "vape client", "vape v4", "vapev4", "vape lite", "bastard", "fuckwit", "arsehole", "asshole", "tit", "twat", "pussy", "cock", "motherfucker", "mother fucker", "wanker");
         for (String s : notgudwords) {
             if (message.toLowerCase().contains(s)) {
 
                 // This is where the message gets cancelled and a message gets sent to the player
 
                 event.setCancelled(true);
-                player.sendMessage(translate("&cSpiceCF &7/ &fYour message &a'"+ message +"' &fwas flagged as not appropriate for this server, it was not sent."));
+                player.sendMessage(translate("&cSpiceCF &7/ &fYour message &e'"+ message +"' &fwas flagged as not appropriate for this server, it was not sent."));
                 break;
             }
         }
